6f9368cce3a850e05d6f585b8e1b9c0c364b9d87
Ramon do Rosário <ramondorosario@gmail.com>
2020-08-15 20:18:20 -0300

    Documentação: Adicionado pasta Back-end + Aula 15, com script base para resolução dos exercícios de casa

---
+++ Back-end/Aula 15/Exercícios da Aula/index.js
@@ -0,0 +1,20 @@
+/*
+  Não altere nada ABAIXO disso até o próximo comentário;
+
+  -- Este código permite que tenhamos uma 
+  -- experiência interativa com o usuário;
+  -- Não é necessário entendê-lo neste momento.
+*/
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+/*
+  Não altere nada ACIMA deste comentário;;
+*/
+
+/**
+ * Escreva seu código aqui embaixo;
+ */
